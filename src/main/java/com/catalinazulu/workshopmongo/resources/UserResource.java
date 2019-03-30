8b1d6cfa54de29313c4d9625aeeb2dc9eea6b889
Wagner Pires <wagnerpires@hotmail.com>
2019-03-30 18:29:09 -0300

    Inserção de usuário com POST

--- src/main/java/com/catalinazulu/workshopmongo/resources/UserResource.java
+++ src/main/java/com/catalinazulu/workshopmongo/resources/UserResource.java
@@ -1,16 +1,17 @@
 package com.catalinazulu.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.catalinazulu.workshopmongo.domain.User;
 import com.catalinazulu.workshopmongo.dto.UserDTO;
@@ -33,14 +34,15 @@ public class UserResource {
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET) // or @GetMapping
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		// ZULU PROBLEMA PROFESSOR
-		// User obj = service.findById(id);
-		// return ResponseEntity.ok().body(new UserDTO(obj));
-
-		Optional<User> obj = service.findById(id);
-		
-		return ResponseEntity.ok().body(new UserDTO());
-		//return ResponseEntity.ok().body(new UserDTO(obj));
+		 User obj = service.findById(id);
+		 return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-
+	
+	@RequestMapping(method=RequestMethod.POST) // or @PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
--- src/main/java/com/catalinazulu/workshopmongo/services/UserService.java
+++ src/main/java/com/catalinazulu/workshopmongo/services/UserService.java
@@ -4,35 +4,33 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.catalinazulu.workshopmongo.domain.User;
+import com.catalinazulu.workshopmongo.dto.UserDTO;
 import com.catalinazulu.workshopmongo.respository.UserRepository;
 import com.catalinazulu.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	public Optional<User> findById(String id) {
-		// ZULU PROBLEMA PROFESSOR
-		// User user = repo.findOne(id);
-		
-		Example<User> match = Example.of(new User());
-		Optional<User> result = repo.findOne(match);
-		
-		if (result == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado");
-		}
-		return result;
-		// return user;
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
